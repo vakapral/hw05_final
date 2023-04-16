58ac3776d9fc815848006ea07884e73fbcb11e24
victor.kapral <kapviktor@gmail.com>
2023-04-16 19:44:26 +0300

    preps for review

--- yatube/core/tests.py
+++ yatube/core/tests.py
@@ -1,8 +1,15 @@
+from http import HTTPStatus
+
+from django.core.cache import cache
 from django.test import TestCase
 
 
 class ViewTestClass(TestCase):
+
+    def setUp(self) -> None:
+        cache.clear()
+
     def test_error_page(self):
         response = self.client.get('/nonexist-page/')
-        # Проверьте, что статус ответа сервера - 404
-        # Проверьте, что используется шаблон core/404.html
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        self.assertTemplateUsed(response, 'core/404.html')
