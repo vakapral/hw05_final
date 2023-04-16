a50b22d04d04fb61610c8e32df2b200c8abd54dd
victor.kapral <kapviktor@gmail.com>
2023-04-16 19:50:40 +0300

    preps for review

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -145,10 +145,10 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
     }
-} 
+}
